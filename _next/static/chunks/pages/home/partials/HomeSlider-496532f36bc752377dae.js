c8cf531c02b84ee7c5257d6ea9d23a74f1dd760a
furcan <49978011+furcan@users.noreply.github.com>
2021-08-07 00:22:23 +0000

    Deploying to github-pages from @ notiflix/notiflix.github.io@8d1a139cddcd00248ab5619a179ad23bfd971d0a 🚀

--- _next/static/chunks/pages/home/partials/HomeSlider-496532f36bc752377dae.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[223],{4733:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home/partials/HomeSlider",function(){return u(929)}])}},function(n){n.O(0,[617,764,929,774,888,179],(function(){return _=4733,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
