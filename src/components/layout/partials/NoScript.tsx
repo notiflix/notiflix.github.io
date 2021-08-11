dc70d65a5db9c004496c14352275f9e26399d1e8
Furkan MT <49978011+furcan@users.noreply.github.com>
2021-08-11 16:50:14 +0300

    NoScript: Styles has minified.

--- src/components/layout/partials/NoScript.tsx
+++ src/components/layout/partials/NoScript.tsx
@@ -5,32 +5,7 @@ function NoScript(): JSX.Element {
 
   return (
     <noscript className="noscript">
-      <style>
-        {`
-        .noscript__message {
-          box-sizing: border-box;
-          outline: none;
-          position: fixed;
-          z-index: 9999;
-          left: 0;
-          top: 0;
-          width: 100%;
-          height: 100%;
-          display: flex;
-          flex-wrap: wrap;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          text-align: center;
-          margin: 0;
-          padding: 1.25rem;
-          font-family: sans-serif;
-          font-size: 1rem;
-          color: #f8f8f8;
-          background: #1e1e1e;
-        }
-      `}
-      </style>
+      <style>{`.noscript__message {box-sizing: border-box;outline: none;position: fixed;z-index: 9999;left: 0;top: 0;width: 100%;height: 100%;display: flex;flex-wrap: wrap;flex-direction: column;align-items: center;justify-content: center;text-align: center;margin: 0;padding: 1.25rem;font-family: sans-serif;font-size: 1rem;color: #f8f8f8;background: #1e1e1e;}`}</style>
       <p className="noscript__message">{_dbSettings?.bodyNoScriptMessage}</p>
     </noscript>
   );
