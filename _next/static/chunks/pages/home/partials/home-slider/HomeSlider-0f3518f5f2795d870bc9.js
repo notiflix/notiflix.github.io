dcd9804f12ebb0dada0a5c05999a46faea8d7a44
furcan <49978011+furcan@users.noreply.github.com>
2021-09-04 18:56:51 +0000

    Deploying to github-pages from @ notiflix/notiflix.github.io@17b08ca40f4c740ca3c90bc7e12756ae6a971c79 🚀

---
+++ _next/static/chunks/pages/home/partials/home-slider/HomeSlider-0f3518f5f2795d870bc9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[910],{2546:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home/partials/home-slider/HomeSlider",function(){return e(7458)}])}},function(n){n.O(0,[617,893,458,774,888,179],(function(){return _=2546,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ _next/static/chunks/pages/notify/partials/notify-playground/NotifyPlayground-6de9e62274060c8b2ffe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[505],{7942:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/notify/partials/notify-playground/NotifyPlayground",function(){return _(4116)}])}},function(n){n.O(0,[874,556,893,317,116,774,888,179],(function(){return u=7942,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
