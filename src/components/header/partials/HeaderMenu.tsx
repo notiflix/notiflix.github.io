af754fa7b4d4a57e21ed4038deb12bba40f1bb42
Furkan MT <49978011+furcan@users.noreply.github.com>
2021-09-11 01:52:38 +0300

    HeaderMenu: routes map nullish check.

--- src/components/header/partials/HeaderMenu.tsx
+++ src/components/header/partials/HeaderMenu.tsx
@@ -13,21 +13,21 @@ function HeaderMenu({ mobileMenuOnClickHandler }: IHeaderMenu): JSX.Element {
   return (
     <nav className={styles.nav}>
       <ul className={styles.nav__ul}>
-        {routes?.filter(route => route.isActive && route.showInHeaderMenu)?.sort((x, y) => x.sortOrder - y.sortOrder)?.map(route => {
+        {routes?.filter(route => route?.isActive && route?.showInHeaderMenu)?.sort((x, y) => x?.sortOrder - y?.sortOrder)?.map(route => {
           return (
-            <li key={route.id} className={styles.nav__ul__li}>
-              <Link href={route.pathPage} as={`${process.env.appUrl}${route.pathAs}`} passHref>
+            <li key={route?.id} className={styles.nav__ul__li}>
+              <Link href={route?.pathPage} as={`${process.env.appUrl}${route?.pathAs}`} passHref>
                 <a
-                  aria-label={`${constants.app.name} ${route.name} ${constants.app.text.module}`}
+                  aria-label={`${constants.app.name} ${route?.name} ${constants.app.text.module}`}
                   onClick={() => mobileMenuOnClickHandler(false)}
-                  target={route.isTargetBlank ? '_blank' : undefined}
+                  target={route?.isTargetBlank ? '_blank' : undefined}
                   className={[
                     `${styles.nav__ul__li__a}`,
-                    `${route.isProductPage ? (styles[`nav__ul__li__a--product`] || '') : ''}`,
+                    `${route?.isProductPage ? (styles[`nav__ul__li__a--product`] || '') : ''}`,
                   ].join(' ').trim()}
                 >
-                  {route.IconComponent ? <route.IconComponent /> : ''}
-                  <span>{route.name}</span>
+                  {route?.IconComponent ? <route.IconComponent /> : ''}
+                  <span>{route?.name}</span>
                 </a>
               </Link>
             </li>
