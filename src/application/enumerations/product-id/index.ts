76457074dbfc8b3055aebfce8f675550db7a498d
Furkan MT <49978011+furcan@users.noreply.github.com>
2021-10-01 00:48:15 +0300

    Enum: ProductId, refactor.

--- src/application/enumerations/product-id/index.ts
+++ src/application/enumerations/product-id/index.ts
@@ -6,6 +6,17 @@ enum EProductId {
   BLOCK = 'BLOCK',
 }
 
+const mapProductNameAsString = new Map<string, string>([
+  [EProductId.NOTIFY, `Notify`],
+  [EProductId.REPORT, `Report`],
+  [EProductId.CONFIRM, `Confirm`],
+  [EProductId.LOADING, `Loading`],
+  [EProductId.BLOCK, `Block`],
+]);
+
+const getProductNameAsString = (enumeration: string): string => mapProductNameAsString.get(enumeration) || '';
+
 export {
   EProductId,
+  getProductNameAsString,
 };
